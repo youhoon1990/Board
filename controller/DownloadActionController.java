908b466e72ef31853bbd1be00952b449f6aee2e8
youhoon1990 <71203728+youhoon1990@users.noreply.github.com>
2021-03-01 23:58:51 +0900

    Update DownloadActionController.java

--- controller/DownloadActionController.java
+++ controller/DownloadActionController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class downloadAction
  */
-@WebServlet("/downloadAction")
+@WebServlet("/DownloadAction")
 public class DownloadActionController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
